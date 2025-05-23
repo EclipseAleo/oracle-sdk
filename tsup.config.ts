a76e8420068c5b3650407d37dc9e807559c86519
Risson <aime.risson.1@gmail.com>
2025-05-23 10:49:14 +0200

    fix : package json

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['cjs', 'esm'],   
   dts: true,
+  target: 'es2020',
   splitting: false,
   sourcemap: true,
-  clean: true,
   treeshake: true,
+  clean: true,
   outDir: 'dist',
+
   outExtension({ format }) {
-    return {
-      js: format === 'cjs' ? '.js' : '.mjs',
-    };
+    return { js: format === 'cjs' ? '.cjs' : '.mjs' };
   },
-}); 
+});
